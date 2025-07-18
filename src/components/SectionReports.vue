965b7bdac9476f8ed4809596bc6914842b75d9b5
maxx52 <gospelwork@yandex.ru>
2025-07-19 06:31:15 +0700

    section reports added

--- src/components/SectionReports.vue
+++ src/components/SectionReports.vue
@@ -1,17 +1,23 @@
 <template>
   <section id="reports" class="section-background">
     <v-container>
-      <h2 class="text-center text-h2 mt-8 mb-5 font-weight-bold warning-color">Отчеты и документы</h2>
-      <p class="text-center text-h4">Мы ответственно следим за документацией, и готовы показать всю отчетность</p>
-      <v-row>
-        <span class="mt-4">
-          <v-col md="4">
-            <v-icon size="120" class="warning-color text-center d-inline-block">mdi-finance</v-icon>
-            <p class="text-center d-inline-block mt-3 text-h5">Финансовые отчеты</p>
-          </v-col>
-          <v-col md="4"></v-col>
-          <v-col md="4"></v-col>
-        </span>
+      <section class="mt-10 mb-10">
+        <h2 class="text-center text-h2 mt-8 mb-5 font-weight-bold warning-color">Отчеты и документы</h2>
+        <p class="text-center text-h4">Мы ответственно следим за документацией, и готовы показать всю отчетность</p>
+      </section>
+      <v-row class="mt-10 mb-10">
+        <v-col md="4">
+          <v-icon size="120" class="warning-color d-block ml-auto mr-auto">mdi-finance</v-icon>
+          <p class="text-center d-block mt-5 text-h5">Финансовые отчеты</p>
+        </v-col>
+        <v-col md="4">
+          <v-icon size="120" class="warning-color ml-auto mr-auto d-block">mdi-chart-arc</v-icon>
+          <p class="text-center mt-3 text-h5 d-block mt-5">Годовой отчёт</p>
+        </v-col>
+        <v-col md="4">
+          <v-icon size="120" class="warning-color ml-auto mr-auto d-block">mdi-file-document-outline</v-icon>
+          <p class="text-center mt-3 text-h5 d-block mt-5 text-center">Учредительные документы</p>
+        </v-col>
       </v-row>
     </v-container>
   </section>
